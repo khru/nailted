a5ea7454657a6a6ce70b85536a83b06c2674e0c2
Emmanuel Valverde Ramos <evrtrabajo@gmail.com>
2020-09-14 13:48:31 +0200

    Phase 3: Phone value object

---
+++ api/src/Users/Domain/InvalidPhoneError.ts
@@ -0,0 +1,8 @@
+class InvalidPhoneError extends Error {
+  constructor(message: string) {
+    super(`InvalidPhoneError: ${message}`);
+    Object.setPrototypeOf(this, InvalidPhoneError.prototype);
+  }
+}
+
+export { InvalidPhoneError };
---
+++ api/src/Users/Domain/Phone.ts
@@ -0,0 +1,19 @@
+import { StringAttribute } from './StringAttribute';
+import { InvalidPhoneError } from './InvalidPhoneError';
+
+export class Phone extends StringAttribute {
+  private readonly VALID_PHONE_REGEX: RegExp = new RegExp(/[1-9][0-9]{2}-[0-9]{3}-[0-9]{4}$/gm);
+  readonly phone: string;
+  constructor(phone: string) {
+    super();
+    this.throwErrorIfEmptyAttribute(phone, 'The attribute email can not be empty');
+    this.throwErrorIfInvalidEmail(phone);
+    this.phone = phone;
+  }
+
+  throwErrorIfInvalidEmail(phone: string): void {
+    if (!this.VALID_PHONE_REGEX.test(phone)) {
+      throw new InvalidPhoneError('Invalid format on the phone number');
+    }
+  }
+}
