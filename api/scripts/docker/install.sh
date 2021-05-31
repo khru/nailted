cdd3d5d4cad1b3e476017ab37dfde7d776d961bd
Emmanuel Valverde Ramos <evrtrabajo@gmail.com>
2021-05-31 20:18:17 +0200

    fix: unused script

--- api/scripts/docker/install.sh
+++ api/scripts/docker/install.sh
@@ -3,6 +3,6 @@
 # Getting .env values to use on the script
 CONTAINER_ID=$(grep NODE_CONTAINER_NAME .env | cut -d '=' -f2);
 
-docker exec -it $CONTAINER_ID npm install && npm audit fix;
+docker exec -it $CONTAINER_ID npm install;
 
 return 0;
